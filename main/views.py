f75fab69aa0e4f0fd71a04ad69dd47d89e45f703
fathonidf <daffafathoni@gmail.com>
2024-04-28 22:08:41 +0700

    feat: Profile Dashboard templates, urls, and views

--- main/views.py
+++ main/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
 # Create your views here.
+def show_dashboard(request):
+    context = {
+        'nama': "Daffa Mohamad Fathoni",
+        'email': "daffafathoni@gmail.com",
+        'kota_asal': "Jakarta Timur",
+        'gender': "Male",
+        'tempat_lahir': "Jakarta",
+        'tanggal_lahir': "14 Juni 2004",
+        'role': "User"
+    }
+    return render(request, "dashboard.html", context)
