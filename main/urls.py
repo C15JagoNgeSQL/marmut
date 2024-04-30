c1e7fe411a23f08c63908ee2cb1bbd5c8952700a
fathi.qushoyyi <fathi.ahimsa@gmail.com>
2024-04-30 18:58:46 +0700

    front end dasar dari fitur fathi, belum include punya dafton

---
+++ main/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from main.views import show_main, login_user, logout_user
+
+app_name = 'main'
+
+urlpatterns = [
+    path('', show_main, name='show_main'),
+    path('login/', login_user, name='login'), #sesuaikan dengan nama fungsi yang dibuat'
+    path('logout/', logout_user, name='logout'),
+]
--- main/views.py
+++ main/views.py
@@ -1,3 +1,61 @@
-from django.shortcuts import render
+from pyexpat.errors import messages
+from django.shortcuts import redirect, render
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
 
-# Create your views here.
+@login_required(login_url='/login')
+#Main Page
+def show_main(request):
+    context = {
+        'name': 'C15',
+        'class': 'BASDAT C'
+    }
+
+    return render(request, "main.html", context)
+
+## Create your views here (normal)
+# def login_user(request):
+#    if request.method == 'POST':
+#        username = request.POST.get('username')
+#        user = authenticate(request, username=username, password=password)
+#        if user is not None:
+#            login(request, user)
+#            return redirect('main:show_main')
+#        else:
+#            messages.info(request, 'Sorry, incorrect username or password. Please try again.')
+#    context = {}
+#    return render(request, 'login.html', context)
+
+# Logout (normal)
+def logout_user(request): 
+    logout(request)
+    return redirect('main:login')
+
+
+#For dummy user
+def create_dummy_user(username, password):
+    # Cek apakah pengguna dengan username yang sama sudah ada
+    if not User.objects.filter(username=username).exists():
+        # Buat pengguna baru
+        user = User.objects.create_user(username=username, password=password)
+        # Simpan pengguna
+        user.save()
+
+#Login with dummy user
+def login_user(request):
+    # Membuat dummy user jika tidak ada
+    create_dummy_user('username_dummy', 'password_dummy')
+
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('main:show_main')
+        else:
+            messages.info(request, 'Sorry, incorrect username or password. Please try again.')
+    context = {}
+    return render(request, 'login.html', context)
+
