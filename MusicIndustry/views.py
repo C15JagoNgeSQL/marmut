09f36416d4b1d07570b563ed3f82bfcb2c87a3cd
fathonidf <daffafathoni@gmail.com>
2024-05-02 22:18:12 +0700

    feat: front end dafton

--- MusicIndustry/views.py
+++ MusicIndustry/views.py
@@ -7,7 +7,7 @@ def show_royalti(request):
         'nama': "Taylor Swift",
         'email': "taylorswift@gmail.com",
         'total_keseluruhan_play': 999999,
-        'total_keseluruhan_download': 100000,  # Added comma here
+        'total_keseluruhan_download': 100000,
         'isi_tabel': [
             {
                 'judul_lagu': "I Can Do It With A Broken Heart",
@@ -47,3 +47,59 @@ def show_royalti(request):
         ]
     }
     return render(request, "cek_royalti.html", context)
+
+def kelola_album(request):
+    context = {
+        'nama': "Luke Chiang",
+        'email': "luchiang@gmail.com",
+        'isLabel': False,
+        'kontak': "08123456789",
+        'total_album': 2,
+        'total_lagu': 4,
+        'isi_tabel': [
+            {
+                'judul': "Album 1",
+                'label': "LabelA",
+                'jumlah_lagu': 0,
+                'total_durasi': 0,
+            },
+            {
+                'judul': "Album2",
+                'label': "LabelB",
+                'jumlah_lagu': 2,
+                'total_durasi': 4,
+            }
+        ]
+    }
+    return render(request, "kelola_album.html", context)
+
+def create_album(request):
+    return render(request, "create_album.html")
+
+def daftar_lagu(request):
+    context = {
+        'nama': "Album 1",
+        'total_lagu': 999999,
+        'total_durasi': 100000,
+        'isi_tabel': [
+            {
+                'judul': "Lagu1",
+                'durasi': "2 menit",
+                'total_play': 3,
+                'total_download': 0,
+            },
+            {
+                'judul': "Lagu1",
+                'durasi': "3 menit",
+                'total_play': 2,
+                'total_download': 2,
+            }
+        ]
+    }
+    return render(request, "daftar_lagu.html", context)
+
+def create_lagu(request):
+    context = {
+        'isSongwriter' : False
+    }
+    return render(request, "create_lagu.html", context)
