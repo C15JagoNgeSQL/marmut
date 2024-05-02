6027917cf2fb89d5b7f171658b0838e18bfae75f
rafizia <mohrafiziaulhaq@gmail.com>
2024-05-02 22:56:48 +0700

    Add Play Song, Play User Playlist

--- PlaylistsAndSongs/urls.py
+++ PlaylistsAndSongs/urls.py
@@ -1,17 +1,15 @@
 from django.urls import path
-from main import views
-from main.views import show_main, login_user, logout_user, show_dashboard
-from main.views import *
-from main.views import temporary_chart_detail_view, temporary_lihat_chart_view, temporary_crud_user_playlist_view, temporary_r_podcast_view
-from PlaylistsAndSongs.views import show_kelola_playlist, show_tambah_playlist, show_ubah_playlist, show_detail_playlist, show_tambah_lagu
+from PlaylistsAndSongs.views import *
 
 app_name = 'PlaylistAndSongs'
 
 urlpatterns = [
-    path('login/', login_user, name='login'),
     path('kelola-playlist/', show_kelola_playlist, name='kelola_playlist'),
     path('tambah-playlist/', show_tambah_playlist, name='tambah_playlist'),
     path('ubah-playlist/', show_ubah_playlist, name='ubah_playlist'),
     path('detail-playlist/', show_detail_playlist, name='detail_playlist'),
     path('tambah-lagu/', show_tambah_lagu, name='tambah_lagu'),
+    path('detail-lagu/', show_detail_lagu, name='detail_lagu'),
+    path('play-user-playlist/', show_play_user_playlist, name='play_user_playlist'),
+    path('tambah-lagu-ke-playlist/', show_tambah_lagu_ke_playlist, name='tambah_lagu_ke_playlist'),
 ]
--- PlaylistsAndSongs/views.py
+++ PlaylistsAndSongs/views.py
@@ -54,3 +54,50 @@ def show_tambah_lagu(request):
         ('2', 'Hotline Bling - Michael Wade')
     ]
     return render(request, "tambahLagu.html", {'songs': songs})
+
+def show_detail_lagu(request):
+    detail = {
+        'judul': "Hotline Bling",
+        'genres': ["Hip Hop", "Pop"],
+        'artist': "Michael Wade",
+        'songwriters': ["Dustin Cantu", "Timothy Mcdaniel", "Jake Franklin"],
+        'durasi': "3",
+        'tanggal_rilis': "27/03/2022",
+        'tahun': "2022",
+        'total_play': "0",
+        'total_download': "0",
+        'album': "Spectral Reverie"
+    }
+    return render(request, "detailLagu.html", {'detail': detail})
+
+def show_tambah_lagu_ke_playlist(request):
+    song = {
+        'judul': "Hotline Bling",
+        'artist': "Michael Wade",
+        'playlists': ["Morning Vibes", "Workout Beats"]
+    }
+    return render(request, "tambah_lagu_ke_playlist.html", {'song': song})
+
+def show_play_user_playlist(request):
+    detail = {
+        'judul': "Morning Vibes",
+        'pembuat': "Rafi",
+        'jumlah_lagu': "5",
+        'total_durasi': "44 menit",
+        'tanggal_dibuat': "26/03/2024",
+        'deskripsi': "Start your day with these uplifting tunes!",
+        'songs': [
+            {
+                'judul': "See You Again",
+                'nama_artist': "Valerie Jordan",
+                'durasi': "5"
+            },
+            {
+                'judul': "Hotline Bling",
+                'nama_artist': "Michael Wade",
+                'durasi': "3"
+            }
+        ]
+    }
+    return render(request, "play_user_playlist.html", {'detail': detail})
+
